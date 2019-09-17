7a87a1e1e7f97140d67f9beec5bed53faa961814
Jerald Gutierrez <jerald.gutierrez@thinkconveyancing.com.au>
2019-09-17 10:35:19 +0800

    Modified newly layout for carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,52 @@
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+//
+// /* $("body").on("click", "#navbarNav li a", function(){
+//   var target = this.hash; // gets the #hash
+//   $target = $(target); //
+//   $('html, body').stop().animate({
+//       'scrollTop': $target.offset().top-80 // scrolls to the link
+//   }, 500, 'swing', function () {
+//   });
+// }); */
+// orientation();
+// function orientation(){
+//   var isOrient = $( "#navbarNav" ).is( ":visible" );
+//   if(isOrient){
+//     $(window).scroll(onScroll);
+//   }
+// }
+// function onScroll(event){
+//   if (window.pageYOffset >= sticky) {
+//   navbar.classList.add("stickyNav");
+//   } else {
+//   navbar.classList.remove("stickyNav");
+//   }
+//   var scrollPosition = $(document).scrollTop();
+//   $('#navbarNav ul li a').each(function () {
+//     var currentLink = $(this);
+//     var refElement = $(currentLink.attr("href"));
+//     if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+//         $('#newTeamSideBar ul liv a.active').removeClass('active');
+//         $(this).addClass('active');
+//     }
+//     else{
+//         currentLink.removeClass("active");
+//       }
+//   });
+// }
 
-/* $("body").on("click", "#navbarNav li a", function(){
-  var target = this.hash; // gets the #hash
-  $target = $(target); //
-  $('html, body').stop().animate({
-      'scrollTop': $target.offset().top-80 // scrolls to the link
-  }, 500, 'swing', function () {
-  });
-}); */
-orientation();
-function orientation(){
-  var isOrient = $( "#navbarNav" ).is( ":visible" );
-  if(isOrient){
-    $(window).scroll(onScroll);
-  }
+// Custom Carousel
+var carouselElem = $(".tss-gallery-holder");
+if ( carouselElem.is(":visible") ) {
+    carouselElem.find('#tssiGallery').lightSlider({
+        gallery: true,
+        item: 1,
+        loop: true,
+        thumbItem: 9,
+        slideMargin: 0,
+        enableDrag: false,
+        auto: true,
+    });
 }
-function onScroll(event){
-  if (window.pageYOffset >= sticky) {
-  navbar.classList.add("stickyNav");
-  } else {
-  navbar.classList.remove("stickyNav");
-  }
-  var scrollPosition = $(document).scrollTop();
-  $('#navbarNav ul li a').each(function () {
-    var currentLink = $(this);
-    var refElement = $(currentLink.attr("href"));
-    if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-        $('#newTeamSideBar ul liv a.active').removeClass('active');
-        $(this).addClass('active');
-    }
-    else{
-        currentLink.removeClass("active");
-      }
-  });
-}
+// Custom Carousel
