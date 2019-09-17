cf93be7b20593a794360c11d7b6cd16dfacb86a0
Narish Tatlonghari <narish.tatlonghari@thinkconveyancing.com.au>
2019-09-17 13:13:06 +0800

    hide nav after click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 // Add smooth scrolling to all links
-$("a").on('click', function(event) {
+$("#navbarTSSIContent li a").on('click', function(event) {
     if (this.hash !== "") {
         event.preventDefault();
         var hash = this.hash;
 
         $('html, body').animate({
             scrollTop: $(hash).offset().top
-        }, 500, function(){
+        }, 800, function(){
             window.location.hash = hash;
+            $(".navbar .navbar-collapse").removeClass("show");
         });
     } // End if
 });
